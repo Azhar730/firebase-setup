b79eb93b7a334acdb3498475d2b5cae665c4aa66
Azhar730 <azharmahmud730@gmail.com>
2024-04-08 19:30:27 +0600

    basic react setupn done

--- src/ErrorElement.jsx
+++ src/ErrorElement.jsx
@@ -1,7 +1,7 @@
 const ErrorElement = () => {
     return (
         <div>
-            <h2 className="text-2xl font-bold text-center">OOPS!!!</h2>            
+            <h2 className="text-7xl mt-56 font-bold text-center">OOPS!!!</h2>            
         </div>
     );
 };
