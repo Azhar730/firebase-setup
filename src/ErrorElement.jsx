46ab4713485fbea26154c20b19eeb139b0531df2
Azhar730 <azharmahmud730@gmail.com>
2024-04-08 18:19:11 +0600

    react-router-dom setup done

---
+++ src/ErrorElement.jsx
@@ -0,0 +1,9 @@
+const ErrorElement = () => {
+    return (
+        <div>
+            <h2 className="text-2xl font-bold text-center">OOPS!!!</h2>            
+        </div>
+    );
+};
+
+export default ErrorElement;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Home';
+import ErrorElement from './ErrorElement';
+import Root from './components/Root';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorElement></ErrorElement>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
